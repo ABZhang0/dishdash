9e20c9218a32e5679e808f79fdcbfd6231fec904
abzhang <a.b.zhang@outlook.com>
2019-08-29 11:27:28 -0700

    unique keys added for list items

--- dishdash-ui/src/App.js
+++ dishdash-ui/src/App.js
@@ -107,7 +107,7 @@ class App extends Component {
               onChange={ this.onInputChange } onKeyDown={ this.onSearchKey } onKeyUp={ this.checkClearSuggestions } spellCheck="false"/>
             <button className="Search-button" style={ searchButtonStyle } onClick={ this.onSearchClick }>Search</button>
             <ul>
-              { this.state.searchSuggestions.map(item => <li onClick={ () => { this.searchAutoFill(item) } }>{ item }</li>) }
+              { this.state.searchSuggestions.map((item, index) => <li key={ index } onClick={ () => { this.searchAutoFill(item) } }>{ item }</li>) }
             </ul>
           </div>
           <CSSTransition in={ this.state.displayDishes } timeout={ 500 } classNames="Dishes-transition">
